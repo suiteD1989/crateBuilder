b382395764cab3bba34f45598db5a9dc5b291728
Daragh Cassidy <daraghcassidy89@gmail.com>
2017-02-23 23:39:15 +0000

    Add files via upload

---
+++ crate-builder/js/main.controller.js
@@ -0,0 +1,56 @@
+  var app = angular.module('exampleApp', ['spotify', 'ui.bootstrap']);
+      app.config(function (SpotifyProvider) {
+        SpotifyProvider.setClientId('4295e67eebcb438f830d8ff9cf44e553');
+        SpotifyProvider.setRedirectUri('http://localhost/angular-spotify/crate-builder/callback.html');
+        SpotifyProvider.setScope('playlist-read-private');
+    });
+
+  app.controller('MainController', ['$scope', '$http', 'Spotify', function ($scope, $http, Spotify) { 
+
+  $scope.login = function() {
+    Spotify.login().then(function (data) {
+      console.log(data);
+      alert("You are now logged in");
+    }, function () {
+      console.log('didn\'t log in');
+    })
+  };
+
+  $scope.getSavedUserTracks = function() {
+    Spotify.getSavedUserTracks().then(function(){
+      $scope.savedtracks = data;
+      console.log(data);
+    });
+  };
+
+  $scope.showUserPlaylists = function() {
+    Spotify.getUserPlaylists($scope.currentUser).then(function (data) {
+      $scope.userplaylist = data.items;
+    });
+  };
+
+  $scope.showPlaylistInfo = function($id) {
+    Spotify.getPlaylistTracks($scope.currentUser, $id).then(function (data) {
+      $scope.playlisttracks = data;
+      $scope.playlistartists = data; 
+    });
+  };
+
+  $scope.getCurrentUser = function() {
+    Spotify.getCurrentUser().then(function (data) {
+      $scope.currentUser = data.id;
+    });
+  };
+
+  $scope.discogsQuery = function($queryArtist, $queryTrack) {
+    $http.get('https://api.discogs.com/database/search?q=' + $queryArtist  + ' ' + $queryTrack.substring(0, $queryTrack.indexOf('-')), 
+    {headers: { Authorization: ' Discogs key=rJIwIgahhwnMAiEgnKUm, secret=UMOtsLfpgZhbXRJKlELmKXmeFESmBYzN '}})
+    .then(function(response) {
+      console.log(response);       
+      
+      $scope.purchaseURL = 'www.discogs.com' + response.data.results[1].uri;
+      console.log(response.data.results[1].uri);
+    });
+  };
+
+}]);
